bd0bbf37801211ec71a3d812524caf2b40d254f1
josehb96 <josehurtadobaeza@gmail.com>
2024-08-21 20:42:42 +0200

    v-if, v-else & v-else-if Directives

--- vue-crash-2024/src/App.vue
+++ vue-crash-2024/src/App.vue
@@ -1,3 +1,17 @@
+<script>
+  export default {
+    data() {
+      return {
+        name: 'John Doe',
+        status: 'pending', 
+      }
+    }
+  }
+</script>
+
 <template>
   <h1>Vue Jobs</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
 </template>
