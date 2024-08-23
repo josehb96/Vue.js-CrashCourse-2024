894b09e5f4732bf2ff2452f8496ae86d0ea23e33
josehb96 <josehurtadobaeza@gmail.com>
2024-08-23 20:57:14 +0200

    ref() & Reactive Values AND Composition API Short Form

--- vue-crash-2024/src/App.vue
+++ vue-crash-2024/src/App.vue
@@ -1,31 +1,20 @@
-<script>
-export default {
-  setup(){
-  
-    const name = 'John Doe';
-    const status = 'active';
-    const tasks = ['Task One', 'Task Two', 'Task Three'];
+<script setup> 
+import { ref } from 'vue';
+
+    const name = ref('John Doe');
+    const status = ref('active');
+    const tasks = ref(['Task One', 'Task Two', 'Task Three']);
   
     const toggleStatus = () => {
-      if (this.status === 'active') {
-        this.status = 'pending';
-      } else if (this.status === 'pending') {
-        this.status = 'inactive';
+    if (status.value === 'active') {
+        status.value = 'pending';
+      } else if (status.value === 'pending') {
+        status.value = 'inactive';
       } else {
-        this.status = 'active';
+        status.value = 'active';
       }
     };
-  
-    return {
-      name,
-      status,
-      tasks,
-      toggleStatus,
-    };
 
-  },
-
-};
 </script>
 
 <template>
